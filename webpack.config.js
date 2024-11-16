6458bc561982b249675dba68bd03e2c2387e52c3
Mks <mukundsrinivasan0@gmail.com>
2024-11-16 22:39:20 +1100

    added yaml, moved index to dist

--- webpack.config.js
+++ webpack.config.js
@@ -23,3 +23,26 @@ module.exports = {
         static: './dist',
     }
 };
+
+// const path = require('path');
+
+// module.exports = {
+//   entry: './src/index.ts',  // Your entry TypeScript file
+//   output: {
+//     filename: 'bundle.js',  // Output file
+//     path: path.resolve(__dirname, 'dist'),  // Output folder
+//   },
+//   resolve: {
+//     extensions: ['.ts', '.js'],  // Resolve TypeScript and JavaScript files
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.ts$/,  // Apply to .ts files
+//         use: 'ts-loader',  // Use TypeScript loader
+//         exclude: /node_modules/,
+//       },
+//     ],
+//   },
+//   devtool: 'source-map',  // Optional: helps with debugging
+// };
