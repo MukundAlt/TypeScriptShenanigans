2be94f2360bf6f0a59f50d92acb074b8695b5317
Mks <mukundsrinivasan0@gmail.com>
2024-11-16 23:42:43 +1100

    changes webpack to prod

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    mode: "development",
+    mode: "production",
     entry: './src/index.ts',
     module: {
         rules: [
@@ -24,26 +24,3 @@ module.exports = {
         static: './dist',
     }
 };
-
-// const path = require('path');
-
-// module.exports = {
-//   entry: './src/index.ts',  // Your entry TypeScript file
-//   output: {
-//     filename: 'bundle.js',  // Output file
-//     path: path.resolve(__dirname, 'dist'),  // Output folder
-//   },
-//   resolve: {
-//     extensions: ['.ts', '.js'],  // Resolve TypeScript and JavaScript files
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.ts$/,  // Apply to .ts files
-//         use: 'ts-loader',  // Use TypeScript loader
-//         exclude: /node_modules/,
-//       },
-//     ],
-//   },
-//   devtool: 'source-map',  // Optional: helps with debugging
-// };
